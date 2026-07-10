a49b22e7ae13cff0ae609a73c8a4034778c4eedc
Abhijeet Srivastava <abhijeetsrivastava@Abhijeets-MacBook-Air.local>
2026-07-11 02:14:31 +0530

    fix: remove Express backend folder from repo, API routes live in Next.js

--- backend/models/Team.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const teamSchema = new mongoose.Schema({
-  img:  { type: String, required: true },
-  name: { type: String, required: true },
-  role: { type: String, required: true },
-}, { timestamps: true })
-
-module.exports = mongoose.model('Team', teamSchema)
--- backend/models/Testimonial.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const testimonialSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  loc:  { type: String, required: true },
-  text: { type: String, required: true },
-}, { timestamps: true })
-
-module.exports = mongoose.model('Testimonial', testimonialSchema)
